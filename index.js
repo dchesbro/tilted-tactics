5870f978069e8928074e06a895efa6bfc813e219
Dwight Chesbro <dchesbro@gmail.com>
2023-03-16 09:05:02 -0700

    First draft.

---
+++ index.js
@@ -0,0 +1,12 @@
+import { strategies } from "./src/strategies";
+
+function random(max, min) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+export default function tactic() {
+  const e = random((strategies.length - 1), 0);
+  const t = random((strategies[e].strategies.length - 1), 0);
+
+  return `${strategies[e].strategies[t]} (${strategies[e].name}, ${strategies[e].year})`;
+}
